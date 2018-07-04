82e00121a8a2e3f0abba847aea53d185cdc16b9b
Sebastien Renaut <sebastien.renaut@gmail.com>
2018-07-04 16:07:04 -0400

    minor

--- Rcode/stats.R
+++ Rcode/stats.R
@@ -7,6 +7,7 @@ library(nlme)
 library(lme4)
 library(ape)
 library(pals)
+library(RVAideMemoire)
 
 ###Preparing OTU table and sampling design----
 OTU = read.table("results/OTU.table.VT.merged.ordered.top.tsv",row.names = 1, sep = "\t",header =T,stringsAsFactors = F)
@@ -31,41 +32,7 @@ design = design[order(rownames(design)),]
 keep = rownames(design) %in% rownames(OTU.norm)
 design.keep = design[keep,]
 
-###VTX00113 ----
-#This is the most abundant VTX, and a Glomus spp,so...
-#Is R. irregularis (VTX00113) more abundant in inoculated soils?
-
-#prepare a matrix with only VTX00113 
-OTU.norm.VTX00113 = cbind(sqrt(OTU.norm[,1]),design.keep)
-colnames(OTU.norm.VTX00113)[1] = colnames(OTU.norm)[1]
-
-#linear mixed effect model (block is random)
-lmm1 <- lme(VTX00113~treatment+species+growing_stage,data = OTU.norm.VTX00113,random = ~1|bloc, method = "ML")
-anova(lmm1)
-
-#              numDF denDF  F-value p-value
-#Intercept)       1   102 316.02848  <.0001
-#treatment         1     8   0.25388  0.6279
-#species           2     8  32.53176  0.0001 #only significant effect
-#growing_stage     1   102   2.78624  0.0981
-#
-
-shapiro.test(lmm1$residuals) #normaly distributed with the square root transform.
-
-
-#boxplots
-dev.new()
-par(mfrow = c(2,2))
-boxplot(OTU.norm.VTX00113[OTU.norm.VTX00113[,2]=="Wheat",1]~OTU.norm.VTX00113[OTU.norm.VTX00113[,2]=="Wheat",5],main = "VTX00113 - Wheat",ylab = "OTU relative abundance",names = expression(italic(control),italic(inoculated)))
-boxplot(OTU.norm.VTX00113[OTU.norm.VTX00113[,2]=="Corn",1]~OTU.norm.VTX00113[OTU.norm.VTX00113[,2]=="Corn",5],main = "VTX00113 - Corn",ylab = "OTU relative abundance",names = expression(italic(control),italic(inoculated)))
-boxplot(OTU.norm.VTX00113[OTU.norm.VTX00113[,2]=="Soy",1]~OTU.norm.VTX00113[OTU.norm.VTX00113[,2]=="Soy",5],main = "VTX00113 - Soy",ylab = "OTU relative abundance",names = expression(italic(control),italic(inoculated)))
-dev.print(device=pdf, "figures/figure1_VTX00113.pdf", onefile=FALSE)
-dev.off()
-
-#figure legends
-system("echo 'Figure 1: Relative abundance of the most abundant Virtual Taxa (VTX00113 representing Rhizophagus irregularis, 33% of all reads) in all three species (wheat, Corn, Soy) tested.' >figures/legends")
-
-###barplots of all VTX to look at diversity ----
+###barplots of all VTX to look at diversity among species and inoculation----
 #first group things according to treatment and species.
 OTU.norm.barplot = cbind(colMeans(OTU.norm[design.keep[,1] == "Wheat" & design.keep[,4] == "inoculated", ]), 
       colMeans(OTU.norm[design.keep[,1] == "Wheat" & design.keep[,4] == "control", ]),
@@ -82,51 +49,54 @@ par(mar=c(16,4,4,2))
 barplot(OTU.norm.barplot,beside = F,font = 3, axisnames = T,ylab = "Relative OTU abundance",col = c(cols25(),rep("grey",60)), las = 3, xpd = T,names.arg =  rep(c("Inoculated","Control"),3),space = 0.05)
 text(y = rep(1.08,6), x = c(1.1,3.2,5.2), labels = c("Wheat","Corn","Soy"), font = 2,cex = 2,xpd = T)
 legend(0.1,-0.4,fill = cols25(),legend = paste(rownames(OTU.norm.barplot)[1:10],taxo[1:10],sep = "_"),cex = 0.75,xpd =T)
-dev.print(device=pdf, "figures/figure2_OTUabundance.pdf", onefile=FALSE)
+dev.print(device=pdf, "figures/figure1_OTUabundance.pdf", onefile=FALSE)
 dev.off()
 
-#figure legends
-system("echo 'Figure 2: mean Relative abundance of the Virtual Taxa per treatment and species' >>figures/legends")
+#print figure caption
+system("echo 'Figure 1: mean Relative abundance of the Virtual Taxa per treatment and species' >figures/legends")
 
-##barplot by genera / order / family ----
-
-#get info from the taxonomy.
+###barplot by genera / order / family ----
+#get taxonomy info.
 OTU.norm.barplot.taxo = as.data.frame(OTU.norm.barplot)
 OTU.norm.barplot.taxo$genera = unlist(strsplit(taxo,";"))[seq(4,(length(taxo)*5),by = 5)]
 OTU.norm.barplot.taxo$order = unlist(strsplit(taxo,";"))[seq(3,(length(taxo)*5),by = 5)]
 OTU.norm.barplot.taxo$family = unlist(strsplit(taxo,";"))[seq(2,(length(taxo)*5),by = 5)]
-
 
 #summarize with dplyr.
 OTU.norm.barplot.taxo_summary.GENERA = OTU.norm.barplot.taxo[,-c(8,9)] %>% group_by(genera) %>% summarise_all(sum)
 OTU.norm.barplot.taxo_summary.ORDER = OTU.norm.barplot.taxo[,-c(7,9)] %>% group_by(order) %>% summarise_all(sum)
 OTU.norm.barplot.taxo_summary.FAMILY = OTU.norm.barplot.taxo[,-c(7,8)] %>% group_by(family) %>% summarise_all(sum)
 
-
+#barplots according to genera/order/family
 dev.new()
+par(mar= c(6,4,4,2))
+
 #genera
-barplot(as.matrix(as.data.frame(OTU.norm.barplot.taxo_summary.GENERA)[,2:7]),xpd = T,
-        names.arg =  colnames(OTU.norm.barplot.taxo_summary.GENERA)[2:7],
-        ylab = "Relative OTU abundance",beside = F,cex.names = 0.5,las = 3,font =3,
+g = barplot(as.matrix(as.data.frame(OTU.norm.barplot.taxo_summary.GENERA)[,2:7]),
+        ylab = "Relative OTU abundance",beside = F,names.arg = rep("",6),
         col = c(cols25()[1:6],rep("grey",60)),xlim = c(0,18*1.2),space = c(0.1))
 
 #order
-barplot(as.matrix(as.data.frame(OTU.norm.barplot.taxo_summary.ORDER)[,2:7]),
-        las = 3,names.arg =  colnames(OTU.norm.barplot.taxo_summary.GENERA)[2:7],cex.names = 0.5,font =3,
+o = barplot(as.matrix(as.data.frame(OTU.norm.barplot.taxo_summary.ORDER)[,2:7]),names.arg = rep("",6),
         add=T,beside = F,col = c(cols25()[7:11],rep("grey",60)),space = c(1.1*6.5,rep(0.1,5)))
 
 #family
-barplot(as.matrix(as.data.frame(OTU.norm.barplot.taxo_summary.FAMILY)[,2:7]),
-        las = 3,names.arg =  colnames(OTU.norm.barplot.taxo_summary.GENERA)[2:7],cex.names = 0.5,font =3,
+f = barplot(as.matrix(as.data.frame(OTU.norm.barplot.taxo_summary.FAMILY)[,2:7]),names.arg = rep("",6),
         add=T,beside = F,col = c(cols25()[12:13],rep("grey",60)),space = c(1.1*13-0.1,rep(0.1,5)))
 
+#add x axix labels
+axis(1,at = c(g,o,f), labels = rep(colnames(OTU.norm.barplot.taxo_summary.GENERA)[2:7],3),font = 3, las = 3,cex.axis=0.8)
+
+#add legends
 legend(0.5,0.9,legend = OTU.norm.barplot.taxo_summary.GENERA$genera,fill = cols25()[1:6],cex = 0.7,bg = "white")
 legend(7.3,0.9,legend = OTU.norm.barplot.taxo_summary.ORDER$order,fill = cols25()[7:11],cex = 0.7,bg = "white")
 legend(14.5,0.9,legend = OTU.norm.barplot.taxo_summary.FAMILY$family,fill = cols25()[12:13],cex = 0.7,bg = "white")
 text(y = rep(1.04,6), x = c(3.3,10.5,17.5), labels = c("Genera","Order","Family"), font = 2,cex = 2,xpd = T)
-
-dev.print(device=pdf, "figures/figure2b_OTUabundance_per_genera_order_family.pdf", onefile=FALSE)
+dev.print(device=pdf, "figures/figure1b_OTUabundance_per_genera_order_family.pdf", onefile=FALSE)
 dev.off()
+
+#print figure caption
+system("echo 'Figure 1b: mean Relative abundance of the Virtual Taxa grouped by genera, order, family per treatment and species' >>figures/legends")
 
 ###alpha diversity ----
 #prepare a matrix with alpha diversity as "invsimpson" index
@@ -159,15 +129,15 @@ boxplot(OTU.norm.alpha$alpha~OTU.norm.alpha$species,beside = F,font = 3, axisnam
 #growing_stage
 boxplot(OTU.norm.alpha$alpha~OTU.norm.alpha$growing_stage,beside = F,font = 3, axisnames = T,ylab = expression(italic(alpha)~~diversity),las = 3, xpd = T,main = "Growing stage",cex.lab = 1.5,cex.main = 1.5,names = expression(italic(early),italic(late)))
 
-dev.print(device=pdf, "figures/figure3_alpha.pdf", onefile=FALSE)
+dev.print(device=pdf, "figures/figure2_alpha.pdf", onefile=FALSE)
 dev.off()
 
 #figure legends
-system("echo 'Figure 3: alpha diversity per Treatment / Species and Growing stage' >>figures/legends")
+system("echo 'Figure 2: alpha diversity per Treatment / Species and Growing stage' >>figures/legends")
 
 
-###beta diversity ----
-#raw data OR normalized data?
+
+###PERMANOVA: are community different (beta diversity) according to sampling design  ----
 #note that results are essentially the same irrespective of prior normalization (OTU.norm) or not (OTU).
 
 #standardization using hellinger transform
@@ -175,17 +145,17 @@ OTU.norm.hel <-decostand(OTU, "hel")
 
 #PERMANOVA (I only keep the species*growing_stage interaction as the other interactions were all NS)
 #The only assumption of PERMANOVA is independence of samples (I think, but could be wrong here)
-adonis(formula=OTU.norm.hel ~ species*growing_stage+treatment+bloc, data=design.keep, permutations=999, method="bray")
+adonis(formula=OTU.norm.hel ~ species*growing_stage+treatment+bloc, data=design.keep, permutations=9999, method="bray")
 
 #                          Df SumsOfSqs MeanSqs F.Model      R2 Pr(>F)    
-#  species                 2    8.2220  4.1110  47.421 0.43650  0.001 ***
-#  growing_stage           1    0.3395  0.3395   3.916 0.01802  0.003 ** 
-#  treatment               1    0.0448  0.0448   0.517 0.00238  0.854    
-#  bloc                    8    1.2314  0.1539   1.776 0.06537  0.003 ** 
-#  species:growing_stage   1    0.2425  0.2425   2.797 0.01287  0.016 *  
-#  Residuals             101    8.7559  0.0867         0.46485           
-#  Total                 114   18.8362                 1.00000           
-#---
+#species                 2    8.2220  4.1110  47.421 0.43650 0.0001 ***
+#growing_stage           1    0.3395  0.3395   3.916 0.01802 0.0014 ** 
+#treatment               1    0.0448  0.0448   0.517 0.00238 0.8526    
+#bloc                    8    1.2314  0.1539   1.776 0.06537 0.0017 ** 
+#species:growing_stage   1    0.2425  0.2425   2.797 0.01287 0.0127 *  
+#Residuals             101    8.7559  0.0867         0.46485           
+#Total                 114   18.8362                 1.00000           
+---
 #  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
 ##Species generate VERY different microbial communities (r2=0.47)
@@ -195,12 +165,13 @@ adonis(formula=OTU.norm.hel ~ species*growing_stage+treatment+bloc, data=design.
 #However, the pcoa reveals that the impact of growth stage could also only be due to the fact that soybeans have only been sampled at early stage,
 #So we will re-run a permanova with only the wheat and the corn samples
 
-####beta diversity per species to look at growth stage?----
-#TO DO
-#TO DO
-#TO DO
+#### posthoc permanova to look at species + growing stage effect----
+#beta diversity per species to look at growth stage?----
 
-####PcoA ----
+#use species AND growing stage as factors (9999 perms for more precision given that one p is close to 0.05) 
+pairwise.perm.manova(dist(OTU,method = "euclidean"),fact = paste(design.keep[,1],design.keep[,2],sep = "_"),nperm = 9999)
+
+####PcoA: good to illustrate the community structure----
 #Calculating Bray-Curtis dissimilarity matrix on the hellinger transformed data
 OTU.norm.hel.bray <-vegdist(OTU.norm.hel, method="bray")
 
@@ -226,11 +197,49 @@ legend(1.15,1.5,fill = rep("transparent",3), border = c("darkred","darkorange","
 
 #inoculation text (this is a pain...)
 text(c(1.37,1.47),c(1.63,1.56),c("inoculated","control"),srt = 45,pos = 3,font =3)
-dev.print(device=pdf, "figures/figure4_pcoa.pdf", onefile=FALSE)
+dev.print(device=pdf, "figures/figure3_pcoa.pdf", onefile=FALSE)
 dev.off()
 
 #figure legends
-system("echo 'Figure 4: PcoA of the all samples colored coded according to species (wheat, corn, soy). Empty / full circles represent the treatment effect' >>figures/legends")
+system("echo 'Figure 3: PcoA of the all samples colored coded according to species (wheat, corn, soy). Empty / full circles represent the treatment effect' >>figures/legends")
+
+
+###VTX00113 ----
+#This is the most abundant VTX, and a Glomus spp,so this is really what we are interested in...
+#Is R. irregularis (VTX00113) more abundant in inoculated soils?
+
+#prepare a matrix with only VTX00113 
+OTU.norm.VTX00113 = cbind(sqrt(OTU.norm[,1]),design.keep)
+colnames(OTU.norm.VTX00113)[1] = colnames(OTU.norm)[1]
+
+#linear mixed effect model (block is random)
+lmm1 <- lme(VTX00113~treatment+species+growing_stage,data = OTU.norm.VTX00113,random = ~1|bloc, method = "ML")
+anova(lmm1)
+
+#              numDF denDF  F-value p-value
+#Intercept)       1   102 316.02848  <.0001
+#treatment         1     8   0.25388  0.6279
+#species           2     8  32.53176  0.0001 #only significant effect
+#growing_stage     1   102   2.78624  0.0981
+#
+
+shapiro.test(lmm1$residuals) #normaly distributed with the square root transform.
+
+#boxplots
+dev.new()
+par(mfrow = c(2,2))
+boxplot(OTU.norm.VTX00113[OTU.norm.VTX00113[,2]=="Wheat",1]~OTU.norm.VTX00113[OTU.norm.VTX00113[,2]=="Wheat",5],main = "VTX00113 - Wheat",ylab = "OTU relative abundance",names = expression(italic(control),italic(inoculated)))
+boxplot(OTU.norm.VTX00113[OTU.norm.VTX00113[,2]=="Corn",1]~OTU.norm.VTX00113[OTU.norm.VTX00113[,2]=="Corn",5],main = "VTX00113 - Corn",ylab = "OTU relative abundance",names = expression(italic(control),italic(inoculated)))
+boxplot(OTU.norm.VTX00113[OTU.norm.VTX00113[,2]=="Soy",1]~OTU.norm.VTX00113[OTU.norm.VTX00113[,2]=="Soy",5],main = "VTX00113 - Soy",ylab = "OTU relative abundance",names = expression(italic(control),italic(inoculated)))
+dev.print(device=pdf, "figures/figure4_VTX00113.pdf", onefile=FALSE)
+dev.off()
+
+#figure legends
+system("echo 'Figure 4: Relative abundance of the most abundant Virtual Taxa (VTX00113 representing Rhizophagus irregularis, 33% of all reads) in all three species (wheat, Corn, Soy) tested.' >>figures/legends")
+
+
+
+
 
 
 ###sandbox ----
